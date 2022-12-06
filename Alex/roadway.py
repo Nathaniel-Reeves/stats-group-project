83381eb836fbfab9de9529cff330ba3a8f5dc42c
cacutler <116305038+cacutler@users.noreply.github.com>
2022-12-06 11:34:54 -0700

    Added roadway file
    Created roadway variables

---
+++ Alex/roadway.py
@@ -0,0 +1,12 @@
+import pandas as pd
+data = pd.read_csv("accident.csv")
+df = pd.DataFrame(data)
+non_motorists = df.PEDS
+motorists = df.PERSONS
+state_highway = df[df.ROUTE == 3]
+federal_highway = df[df.ROUTE == 2]
+interstate = df[df.ROUTENAME == 1]
+county_road = df[df.ROUTE == 4]
+local_street_municipality = df[df.ROUTE == 6]
+local_street_township = df[df.ROUTE == 5]
+local_street_frontage_road = df[df.ROUTE == 7]
