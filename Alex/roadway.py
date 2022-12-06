e07fb7171d12bc2d2432d11906aacbd5f2f64b8d
cacutler <116305038+cacutler@users.noreply.github.com>
2022-12-06 11:47:45 -0700

    Updated roadway file
    Added new variables for motorists and non-motorists

--- Alex/roadway.py
+++ Alex/roadway.py
@@ -1,8 +1,6 @@
 import pandas as pd
 data = pd.read_csv("accident.csv")
 df = pd.DataFrame(data)
-non_motorists = df.PEDS
-motorists = df.PERSONS
 state_highway = df[df.ROUTE == 3]
 federal_highway = df[df.ROUTE == 2]
 interstate = df[df.ROUTENAME == 1]
@@ -12,3 +10,21 @@ local_street_township = df[df.ROUTE == 5]
 local_street_frontage_road = df[df.ROUTE == 7]
 other_road = df[df.ROUTE == 8]
 unknown = df[df.ROUTE == 9]
+motorists_state = state_highway.PERSONS
+motorists_federal = federal_highway.PERSONS
+motorists_interstate = interstate.PERSONS
+motorists_county = county_road.PERSONS
+motorists_municipality = local_street_municipality.PERSONS
+motorists_township = local_street_township.PERSONS
+motorists_frontage = local_street_frontage_road.PERSONS
+motorists_other = other_road.PERSONS
+motorists_unknown = unknown.PERSONS
+nonmotorists_state = state_highway.PEDS
+nonmotorists_federal = federal_highway.PEDS
+nonmotorists_interstate = interstate.PEDS
+nonmotorists_county = county_road.PEDS
+nonmotorists_municipality = local_street_municipality.PEDS
+nonmotorists_township = local_street_township.PEDS
+nonmotorists_frontage = local_street_frontage_road.PEDS
+nonmotorists_other = other_road.PEDS
+nonmotorists_unknown = unknown.PEDS
