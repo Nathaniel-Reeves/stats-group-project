1ef4ef072eead347fb47d4d053b272afa5e2bff3
cacutler <116305038+cacutler@users.noreply.github.com>
2022-12-07 14:53:06 -0700

    Updated variables
    Updated variables for the state of Utah

--- Alex/roadway.py
+++ Alex/roadway.py
@@ -1,15 +1,15 @@
 import pandas as pd
 data = pd.read_csv("accident.csv")
 df = pd.DataFrame(data)
-state_highway = df[df.ROUTE == 3]
-federal_highway = df[df.ROUTE == 2]
-interstate = df[df.ROUTENAME == 1]
-county_road = df[df.ROUTE == 4]
-local_street_municipality = df[df.ROUTE == 6]
-local_street_township = df[df.ROUTE == 5]
-local_street_frontage_road = df[df.ROUTE == 7]
-other_road = df[df.ROUTE == 8]
-unknown = df[df.ROUTE == 9]
+state_highway = df[df.STATE == 49][df.ROUTE == 3]
+federal_highway = df[df.STATE == 49][df.ROUTE == 2]
+interstate = df[df.STATE == 49][df.ROUTENAME == 1]
+county_road = df[df.STATE == 49][df.ROUTE == 4]
+local_street_municipality = df[df.STATE == 49][df.ROUTE == 6]
+local_street_township = df[df.STATE == 49][df.ROUTE == 5]
+local_street_frontage_road = df[df.STATE == 49][df.ROUTE == 7]
+other_road = df[df.STATE == 49][df.ROUTE == 8]
+unknown = df[df.STATE == 49][df.ROUTE == 9]
 motorists_state = state_highway.PERSONS
 motorists_federal = federal_highway.PERSONS
 motorists_interstate = interstate.PERSONS
