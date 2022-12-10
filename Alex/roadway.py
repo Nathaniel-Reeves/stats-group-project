2964c2ee70d2190328d182f28186151cae9330bf
cacutler <116305038+cacutler@users.noreply.github.com>
2022-12-09 21:08:12 -0700

    Updated variables
    Updated variables.  Unable to use 2000 data with variables due to having no data for the specific variables in the 2000 dataset.

--- Alex/roadway.py
+++ Alex/roadway.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-data = pd.read_csv("data/FARS2020NationalCSV/ACCIDENT.csv", encoding_errors='replace')
-data1 = pd.read_csv("data/FARS2000NationalCSV/ACCIDENT.CSV", encoding_errors='replace')
+data = pd.read_csv("accident2020.csv", encoding_errors='replace')
+data1 = pd.read_csv("ACCIDENT2000.csv", encoding_errors='replace')
 df = pd.DataFrame(data)
 df1 = pd.DataFrame(data1)
+#2020 data
 state_highway = df[df.STATE == 49][df.ROUTE == 3]
 federal_highway = df[df.STATE == 49][df.ROUTE == 2]
 interstate = df[df.STATE == 49][df.ROUTE == 1]
@@ -13,25 +14,27 @@ local_street_township = df[df.STATE == 49][df.ROUTE == 5]
 local_street_frontage_road = df[df.STATE == 49][df.ROUTE == 7]
 other_road = df[df.STATE == 49][df.ROUTE == 8]
 unknown = df[df.STATE == 49][df.ROUTE == 9]
-motorists_state = state_highway.PERSONS
-motorists_federal = federal_highway.PERSONS
-motorists_interstate = interstate.PERSONS
-motorists_county = county_road.PERSONS
-motorists_municipality = local_street_municipality.PERSONS
-motorists_township = local_street_township.PERSONS
-motorists_frontage = local_street_frontage_road.PERSONS
-motorists_other = other_road.PERSONS
-motorists_unknown = unknown.PERSONS
-nonmotorists_state = state_highway.PEDS
-nonmotorists_federal = federal_highway.PEDS
-nonmotorists_interstate = interstate.PEDS
-nonmotorists_county = county_road.PEDS
-nonmotorists_municipality = local_street_municipality.PEDS
-nonmotorists_township = local_street_township.PEDS
-nonmotorists_frontage = local_street_frontage_road.PEDS
-nonmotorists_other = other_road.PEDS
-nonmotorists_unknown = unknown.PEDS
+motorists_state = state_highway.PERMVIT
+motorists_federal = federal_highway.PERMVIT
+motorists_interstate = interstate.PERMVIT
+motorists_county = county_road.PERMVIT
+motorists_municipality = local_street_municipality.PERMVIT
+motorists_township = local_street_township.PERMVIT
+motorists_frontage = local_street_frontage_road.PERMVIT
+motorists_other = other_road.PERMVIT
+motorists_unknown = unknown.PERMVIT
+nonmotorists_state = state_highway.PERNOTMVIT
+nonmotorists_federal = federal_highway.PERNOTMVIT
+nonmotorists_interstate = interstate.PERNOTMVIT
+nonmotorists_county = county_road.PERNOTMVIT
+nonmotorists_municipality = local_street_municipality.PERNOTMVIT
+nonmotorists_township = local_street_township.PERNOTMVIT
+nonmotorists_frontage = local_street_frontage_road.PERNOTMVIT
+nonmotorists_other = other_road.PERNOTMVIT
+nonmotorists_unknown = unknown.PERNOTMVIT
 #Make changes to the variables used to see bar graph results
+plt.xticks(ticks = list(range(20)))
+plt.yticks(ticks = list(range(20)))
 plt.xlabel("Number of nonmotorists involved in crashes on unknown Utah roads")
 plt.ylabel("Number of fatalities in crashes on unknown Utah roads")
 plt.bar(nonmotorists_unknown, unknown.FATALS)
