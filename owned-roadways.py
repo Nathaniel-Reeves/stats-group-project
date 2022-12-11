148df43b052689257919e740d4a17b310e34b6b9
Cy-Enderdot <nathaniel.jacob.reeves@gmail.com>
2022-12-11 02:43:56 -0700

    Update owned-roadways.py
    Made the data file easy to read into the codebase.  Percent comment make the code easy to run using jupyter in vs code.

--- owned-roadways.py
+++ owned-roadways.py
@@ -1,6 +1,7 @@
+#%%
 import pandas as pd
 import matplotlib.pyplot as plt
-data = pd.read_csv("accident2020.csv", encoding_errors='replace')
+data = pd.read_csv("data/FARS2020NationalCSV/accident.csv", encoding_errors='replace')
 df = pd.DataFrame(data)
 county_owned = df[df.STATE == 49][df.RD_OWNER == 2]
 state_owned = df[df.STATE == 49][df.RD_OWNER == 1]
@@ -11,3 +12,4 @@ plt.yticks(ticks = list(range(0, 2000, 20)))
 plt.xlabel("Number of fatalities in a crash on privately owned roads in Utah")
 plt.ylabel("Number of crashes involving the specific number of privately on federally owned roads in Utah")
 plt.hist(private_owned.FATALS)
+# %%
